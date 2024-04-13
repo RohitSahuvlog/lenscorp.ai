271a0766c53beb228c8379e3c3ece6c17f54f99c
Rohit Sahu <sahu86744@gmail.com>
2024-04-13 16:50:11 +0530

    update code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import "./47c747d98712b950.css"
+import "./6a76b63ca976b0a2.css"
+import Navbar from './component/navbar';
+import AboutPage from './component/NewAbout';
+import BlogSection from './component/NewBlog';
+import ClientSection from './component/NewClient';
+import NewContact from './component/NewContact';
+import NewFigures from './component/NewFigures';
+import Footer from './component/NewFooter';
+import Newhero from './component/newhero';
+import FAQSection from './component/NewQuestions';
+import Services from './component/NewServices';
+import SolutionsComponent from './component/NewSolutions';
+import NewVision from './component/NewVision';
+import TrueAI from './component/TrueAI';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Newhero />
+      <AboutPage />
+      <Services />
+      <TrueAI />
+      <SolutionsComponent />
+      <NewVision />
+      <BlogSection />
+      <ClientSection />
+      <NewFigures />
+      <FAQSection />
+      <NewContact />
+      <Footer />
     </div>
   );
 }
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,25 +1,42 @@
+
 import React, { useState } from 'react';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <header className="fixed top-0 left-0 w-full flex justify-center items-center py-1 z-50 bg-white transition duration-500">
-            <nav className="flex w-11/12 justify-between items-center">
-                <a href="/" className="flex items-center">
-                    <img src="/_next/static/media/nav_logo.e5fb945a.png" alt="logo" width="80" height="70" className="mt-2 pt-0.5" />
-                </a>
-                <div className="flex flex-col space-y-2 cursor-pointer lg:hidden" onClick={() => setIsOpen(!isOpen)}>
-                    <div className="w-7.5 h-1 bg-blue-500 rounded"></div>
-                    <div className="w-7.5 h-1 bg-blue-500 rounded"></div>
-                    <div className="w-7.5 h-1 bg-blue-500 rounded"></div>
+        <header className="NewNavbar_nav_cont_dark__jxRzF head" style={{ display: "flex" }}>
+            <nav className="NewNavbar_nav_block__Kt2BG">
+                <div>
+                    <a className="imgLink" href="/">
+                        <img alt="logo" loading="lazy" width="80" height="70" decoding="async" data-nimg="1"
+                            style={{ color: "transparent", marginTop: "8px", paddingTop: "2px" }}
+                            srcSet="2Fnav_logo.e5fb945a.png"
+                            src="2Fnav_logo.e5fb945a.png" />
+                    </a>
                 </div>
-                <div className={`${isOpen ? 'flex' : 'hidden'} fixed top-20 right-0 w-72 h-screen flex-col space-y-7 p-6 bg-black text-white lg:static lg:flex lg:w-auto lg:h-auto lg:p-0 lg:bg-transparent lg:space-y-0 lg:space-x-7 lg:flex-row`}>
-                    <a href="https://makemyweb.ai/" target="_blank" rel="noreferrer" className="text-white hover:text-blue-500 lg:text-black">MakeMyWeb.</a>
-                    <a href="/" className="text-white hover:text-blue-500 lg:text-black">Home</a>
-                    <a href="/about" className="text-white hover:text-blue-500 lg:text-black">Company</a>
-                    <a href="/blogs" className="text-white hover:text-blue-500 lg:text-black">Blogs</a>
-                    <button className="flex text-white lg:hidden">Theme</button>
+                <div className="nav__menu-bar" onClick={() => setIsOpen(!isOpen)}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="nav__menu-list">
+                    <div>
+                        <a className="nav__link" target="_blank" style={{ color: "black" }}
+                            href="https://makemyweb.ai/">MakeMyWeb.</a>
+                    </div>
+                    <div>
+                        <a className="nav__link active" style={{ color: "black" }} href="/">Home</a>
+                    </div>
+                    <div>
+                        <a className="nav__link" style={{ color: "black" }} href="/about">Company</a>
+                    </div>
+                    <div>
+                        <a className="nav__link" style={{ color: "black" }} href="/blogs">Blogs</a>
+                    </div>
+                    <button className="themeBtn" style={{ display: "flex" }}>
+                        <span></span>
+                    </button>
                 </div>
             </nav>
         </header>
